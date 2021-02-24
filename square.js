60a802c4f9a84b5568fc1585ceb61c14f5d7af95
farisfadhilah <49354291+farisfadhilah@users.noreply.github.com>
2021-02-24 23:03:21 +0700

    Add ui squares

--- square.js
+++ square.js
@@ -56,33 +56,23 @@ function nearestAnchor(cursorX, cursorY) {
 }
 
 function loadSquareControl() {
-    let dom = document.getElementById('poligon');
-    dom.innerHTML = '<h3>Poligon</h3>'
+    let dom = document.getElementById('square');
+    dom.innerHTML = '<h3>Square</h3>'
     let i = 0;
-    poligons.forEach(pol => {
+    squares.forEach(pol => {
         dom.innerHTML += `<div>
-            jumlah sisi: ${pol.sides}<br>
             <label for="red-${i}">Merah</label>
-            <input type="range" id="red-${i}" min="0" max="100" value="${pol.color[0]*100}" onmousemove="for (let j = 0; j < poligons[${i}].sides; j++) {
-                poligons[${i}].color[j*3] = document.getElementById('red-${i}').value/100;;
+            <input type="range" id="red-${i}" min="0" max="100" value="${pol.color[0]*100}" onmousemove="for (let j = 0; j < squares[${i}].side; j++) {
+                squares[${i}].color[j*3] = document.getElementById('red-${i}').value/100;;
             }"><br>
             <label for="green-${i}">Hijau</label>
-            <input type="range" id="green-${i}" min="0" max="100" value="${pol.color[1]*100}" onmousemove="for (let j = 0; j < poligons[${i}].sides; j++) {
-                poligons[${i}].color[j*3+1] = document.getElementById('green-${i}').value/100;;
+            <input type="range" id="green-${i}" min="0" max="100" value="${pol.color[1]*100}" onmousemove="for (let j = 0; j < squares[${i}].side; j++) {
+                squares[${i}].color[j*3+1] = document.getElementById('green-${i}').value/100;;
             }"><br>
             <label for="blue-${i}">Biru</label>
-            <input type="range" id="blue-${i}" min="0" max="100" value="${pol.color[2]*100}" onmousemove="for (let j = 0; j < poligons[${i}].sides; j++) {
-                poligons[${i}].color[j*3+2] = document.getElementById('blue-${i}').value/100;;
+            <input type="range" id="blue-${i}" min="0" max="100" value="${pol.color[2]*100}" onmousemove="for (let j = 0; j < squares[${i}].side; j++) {
+                squares[${i}].color[j*3+2] = document.getElementById('blue-${i}').value/100;;
             }"><br>
-            <label for="angle-${i}">Sudut (derajat):</label>
-            <input type="number" id="angle-${i}" name="sides" min="0" max="360" value="0">
-            <button onclick="rotatePolygon(poligons[${i}],document.getElementById('angle-${i}').value*2*Math.PI/360)">rotate</button><br>
-            <label for="x-${i}">X :</label>
-            <input type="number" id="x-${i}" name="sides" step="0.01" value="0">
-            <label for="y-${i}">Y :</label>
-            <input type="number" id="y-${i}" name="sides" step="0.01" value="0">
-            <button onclick="translatePolygon(poligons[${i}],[parseFloat(document.getElementById('x-${i}').value), parseFloat(document.getElementById('y-${i}').value), 0])">translate</button><br>
-            <button onclick="poligons.splice(${i},1);loadPoligonControl();">delete</button>
             <hr>
         </div>`
         i++;
@@ -91,8 +81,8 @@ function loadSquareControl() {
     dom.innerHTML += `
     <label for="sides">Jumlah sisi:</label>
     <input type="number" id="sides" name="sides" min="3" value="3">
-    <button onclick="poligons.push(generatePolygon(document.getElementById('sides').value)); loadPoligonControl();">Add Polygon</button>
+    <button onclick="squares.push(generateSquare(document.getElementById('sides').value)); loadSquareControl();">Add Square</button>
     `
 }
 
-// loadSquareControl()
+loadSquareControl()
